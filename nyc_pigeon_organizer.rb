9b915bf70fe5a920a3c56a1b90b8b127c70f6874
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-19 15:28:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-
+new_hash = {}
+data.each do |key, value|
+  value.each do |new_value, names|
+    names.each do |name|
+      
+      if !new_hash[name]
+        new_hash[name] = {}
+      end
+      new_hash[name][key] << new_hash.to_s 
+    end
+  end
+end
+new_hash
 end
